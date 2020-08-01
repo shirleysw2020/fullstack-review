212eead3e2a22517253b48febe5f524a62d8da98
Shirley Wu <shirleyswwu@gmail.com>
2020-07-31 20:27:31 -0700

    fixed binding for search method

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,24 +6,29 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
-  onChange (e) {
+  onChange(e) {
+    console.log('whats props?', this.props)
     this.setState({
       term: e.target.value
     });
+    console.log("whats terms?", this.state.term)
   }
 
   search() {
+    // console.log("whats search's this binding to?", this)
     this.props.onSearch(this.state.term);
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
